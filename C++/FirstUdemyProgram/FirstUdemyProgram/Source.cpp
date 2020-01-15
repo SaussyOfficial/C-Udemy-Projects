5a7b0ca9d53d367154cffd3543a583235a349a8a
ported <55634787+ported-creator@users.noreply.github.com>
2020-01-15 01:17:37 -0500

    Update Source.cpp

--- C++/FirstUdemyProgram/FirstUdemyProgram/Source.cpp
+++ C++/FirstUdemyProgram/FirstUdemyProgram/Source.cpp
@@ -11,11 +11,7 @@ int main()
 	std::cin >> FavoriteNumber;
 
 	std::cout << "Amazing! " << FavoriteNumber << " That's my favorite number too!" << std::endl;
-
-    std::cout << "     " << std::endl;
-
-	std::cout << "Press any key to continue..." << std::endl;
-
+	
 	system("pause");
 	return 0;
 	
