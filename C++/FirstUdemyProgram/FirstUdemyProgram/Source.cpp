c00f3e2eee46aba355a967355bce678ee0570faf
ported <55634787+ported-creator@users.noreply.github.com>
2020-01-16 23:40:23 -0500

    fixed

--- C++/FirstUdemyProgram/FirstUdemyProgram/Source.cpp
+++ C++/FirstUdemyProgram/FirstUdemyProgram/Source.cpp
@@ -6,8 +6,6 @@ int main()
 	int FavoriteNumber{};
 	std::cout << "Enter your favorite number between 1 and 100:";
 
-	std::cout << "     " << std::endl;
-
 	std::cin >> FavoriteNumber;
 
 	std::cout << "Amazing! " << FavoriteNumber << " That's my favorite number too!" << std::endl;
